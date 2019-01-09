315d77f69da9995d05aa9c678364e64e9fe5918e
Adrian Friedli <adrian.friedli@husqvarnagroup.com>
2019-01-09 15:34:25 +0100

    SG-10239 more intuitive ordering of commands

--- recipes-devtools/homekit-devscripts/homekit-devscripts/forgetWifiCredentials.sh
+++ recipes-devtools/homekit-devscripts/homekit-devscripts/forgetWifiCredentials.sh
@@ -3,9 +3,10 @@
 
 set -eu -o pipefail
 
-rm -f /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 systemctl stop wpa_supplicant@wlan0
 ip link set dev wlan0 down
+rm -f /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 ip link set dev wlan0 up
 systemctl start wpa_supplicant@wlan0
+
 systemctl restart network_management@prod
