22c88e7cd233a6ad12bd749ac022a7b97022eded
Reto Schneider <reto.schneider@husqvarnagroup.com>
2019-05-17 13:45:30 +0200

    SG-13447 unique-hostname: Write /etc/hostname atomically

--- recipes-support/unique-hostname/files/unique-hostname.sh
+++ recipes-support/unique-hostname/files/unique-hostname.sh
@@ -5,7 +5,9 @@ set -eu -o pipefail
 if [ "$(cat /etc/hostname)" = "GARDENA" ]; then
     id="$(ip link show dev eth0|grep 'link/ether'|awk '{print $2}'|sed 's/://g'|cut -b7-)"
     if [ -n "$id" ]; then
-        echo "GARDENA-$id" > /etc/hostname
+        echo "GARDENA-$id" > /etc/hostname.tmp
+        sync
+        mv /etc/hostname.tmp /etc/hostname
         hostname -F /etc/hostname
     fi
 fi
