810b10e9d0f8f09f25b788b95b678b9dfd25ab29
Reto Schneider <reto.schneider@husqvarnagroup.com>
2019-05-17 13:47:45 +0200

    SG-13447 unique-hostname: Fix zero-length files

--- recipes-support/unique-hostname/files/unique-hostname.sh
+++ recipes-support/unique-hostname/files/unique-hostname.sh
@@ -2,7 +2,7 @@
 
 set -eu -o pipefail
 
-if [ "$(cat /etc/hostname)" = "GARDENA" ]; then
+if [ "$(cat /etc/hostname)" = "GARDENA" ] || ! [ -s /etc/hostname ]; then
     id="$(ip link show dev eth0|grep 'link/ether'|awk '{print $2}'|sed 's/://g'|cut -b7-)"
     if [ -n "$id" ]; then
         echo "GARDENA-$id" > /etc/hostname.tmp
