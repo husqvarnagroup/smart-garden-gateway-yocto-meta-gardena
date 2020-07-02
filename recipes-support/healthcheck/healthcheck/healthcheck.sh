24466f34a376dc013ba9f998ba14bbe2f58e8504
Reto Schneider <reto.schneider@husqvarnagroup.com>
2020-07-02 02:25:00 +0200

    SG-16002 healthcheck: Ping radio module

--- recipes-support/healthcheck/healthcheck/healthcheck.sh
+++ recipes-support/healthcheck/healthcheck/healthcheck.sh
@@ -201,6 +201,21 @@ test_ppp0() {
     log_result "ppp0" "${result}" "ip_address=${ip_address}"
 }
 
+test_rm_ping() {
+    local result=0
+
+    if ! rm_ip_address="$(fw_printenv -n rmaddr | awk -F: '{print "fc00::6:" $1$2 ":" $3$4 ":" $5$6 }')"; then
+        log_result "rm_ping" "1" "missing rmaddr"
+        return
+    fi
+
+    if ! ping -c1 "${rm_ip_address}" >/dev/null;then
+        result=2
+    fi
+
+    log_result "rm_ping" "${result}" "rm_ip_address=${rm_ip_address}"
+}
+
 test_all() {
     if ping -c1 gateway.iot.sg.dss.husqvarnagroup.net >/dev/null 2>&1 \
        || ping -c1 www.husqvarnagroup.com >/dev/null 2>&1; then
@@ -222,6 +237,7 @@ test_all() {
     test_shadoway_corrupted_directories
     test_systemd_running
     test_ppp0
+    test_rm_ping
 
     return "${something_failed}"
 }
