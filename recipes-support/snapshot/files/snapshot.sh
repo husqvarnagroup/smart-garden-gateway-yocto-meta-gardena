29740938f834d806b78842cc25f8dcd2a27b1c4f
Ezra Buehler <ezra.buehler@husqvarnagroup.com>
2023-01-09 11:11:43 +0100

    SG-19726 snapshot: add gateway ID to folder name
    This allows unpacking snapshots from multiple devices in the same
    directory.

--- recipes-support/snapshot/files/snapshot.sh
+++ recipes-support/snapshot/files/snapshot.sh
@@ -1,54 +1,59 @@
-#!/bin/sh
+#!/bin/ash
+# shellcheck shell=dash
 
-rm -rf /tmp/snapshot
-rm -f /tmp/snapshot.tgz
-mkdir /tmp/snapshot
+gwid=$(/sbin/fw_printenv -n gatewayid)
+readonly SNAPSHOT_DIR="/tmp/${gwid}_snapshot"
+readonly SNAPSHOT_TAR="${SNAPSHOT_DIR}.tar.gz"
+
+rm -rf "${SNAPSHOT_DIR}"
+rm -f "${SNAPSHOT_TAR}"
+mkdir "${SNAPSHOT_DIR}"
 cd /tmp/ || exit 1
 
 ##################################
 # Generic base image information #
 ##################################
-mkdir /tmp/snapshot/etc/
-cp /etc/os-release /tmp/snapshot/etc/
-cp /etc/os-release.old /tmp/snapshot/etc/
-cp /etc/build /tmp/snapshot/etc/
-cp -r /var/lib/sysupgrade /tmp/snapshot/
-cp /etc/resolv.conf /tmp/snapshot/etc/
+mkdir "${SNAPSHOT_DIR}/etc/"
+cp /etc/os-release "${SNAPSHOT_DIR}/etc/"
+cp /etc/os-release.old "${SNAPSHOT_DIR}/etc/"
+cp /etc/build "${SNAPSHOT_DIR}/etc/"
+cp -r /var/lib/sysupgrade "${SNAPSHOT_DIR}/"
+cp /etc/resolv.conf "${SNAPSHOT_DIR}/etc/"
 
 ##################################
 # Relevant runtime information   #
 ##################################
-mkdir /tmp/snapshot/runtime
-/bin/df -h > /tmp/snapshot/runtime/df
-/usr/sbin/ubinfo -a > /tmp/snapshot/runtime/ubinfo
-/sbin/ip address > /tmp/snapshot/runtime/ip-address
-/sbin/ip route > /tmp/snapshot/runtime/ip-route
-/sbin/ip -6 route > /tmp/snapshot/runtime/ip6-route
-/sbin/ip link > /tmp/snapshot/runtime/ip-link
-/bin/journalctl | /bin/gzip > /tmp/snapshot/runtime/journalctl.gz
-/bin/systemctl > /tmp/snapshot/runtime/systemctl
-/bin/systemctl status > /tmp/snapshot/runtime/systemctl-status
-/sbin/fw_printenv > /tmp/snapshot/runtime/fw_printenv
-/bin/date > /tmp/snapshot/runtime/date
-/usr/bin/uptime > /tmp/snapshot/runtime/uptime
-/bin/mount > /tmp/snapshot/runtime/mount
-/bin/netstat -tulen > /tmp/snapshot/runtime/netstat
-/usr/bin/timedatectl > /tmp/snapshot/runtime/timedatectl
-/bin/dmesg > /tmp/snapshot/runtime/dmesg
-/usr/bin/free > /tmp/snapshot/runtime/free
-/usr/sbin/iw wlan0 station dump > /tmp/snapshot/runtime/iw-wlan0-station-dump
-/usr/sbin/iw dev > /tmp/snapshot/runtime/iw-dev
-/bin/ps > /tmp/snapshot/runtime/ps
-/usr/bin/top -bn1 > /tmp/snapshot/runtime/top
-[ -f /usr/sbin/lsof ] && /usr/sbin/lsof > /tmp/snapshot/runtime/lsof
+mkdir "${SNAPSHOT_DIR}/runtime"
+/bin/df -h > "${SNAPSHOT_DIR}/runtime/df"
+/usr/sbin/ubinfo -a > "${SNAPSHOT_DIR}/runtime/ubinfo"
+/sbin/ip address > "${SNAPSHOT_DIR}/runtime/ip-address"
+/sbin/ip route > "${SNAPSHOT_DIR}/runtime/ip-route"
+/sbin/ip -6 route > "${SNAPSHOT_DIR}/runtime/ip6-route"
+/sbin/ip link > "${SNAPSHOT_DIR}/runtime/ip-link"
+/bin/journalctl | /bin/gzip > "${SNAPSHOT_DIR}/runtime/journalctl.gz"
+/bin/systemctl > "${SNAPSHOT_DIR}/runtime/systemctl"
+/bin/systemctl status > "${SNAPSHOT_DIR}/runtime/systemctl-status"
+/sbin/fw_printenv > "${SNAPSHOT_DIR}/runtime/fw_printenv"
+/bin/date > "${SNAPSHOT_DIR}/runtime/date"
+/usr/bin/uptime > "${SNAPSHOT_DIR}/runtime/uptime"
+/bin/mount > "${SNAPSHOT_DIR}/runtime/mount"
+/bin/netstat -tulen > "${SNAPSHOT_DIR}/runtime/netstat"
+/usr/bin/timedatectl > "${SNAPSHOT_DIR}/runtime/timedatectl"
+/bin/dmesg > "${SNAPSHOT_DIR}/runtime/dmesg"
+/usr/bin/free > "${SNAPSHOT_DIR}/runtime/free"
+/usr/sbin/iw wlan0 station dump > "${SNAPSHOT_DIR}/runtime/iw-wlan0-station-dump"
+/usr/sbin/iw dev > "${SNAPSHOT_DIR}/runtime/iw-dev"
+/bin/ps > "${SNAPSHOT_DIR}/runtime/ps"
+/usr/bin/top -bn1 > "${SNAPSHOT_DIR}/runtime/top"
+[ -f /usr/sbin/lsof ] && /usr/sbin/lsof > "${SNAPSHOT_DIR}/runtime/lsof"
 if [ -f /sys/devices/virtual/misc/bootcount/bootcount ]; then
-    cp /sys/devices/virtual/misc/bootcount/bootcount /tmp/snapshot/runtime/bootcount
+    cp /sys/devices/virtual/misc/bootcount/bootcount "${SNAPSHOT_DIR}/runtime/bootcount"
 fi
-/usr/bin/healthcheck  > /tmp/snapshot/runtime/healthcheck 2>&1 || true
-/bin/networkctl > /tmp/snapshot/runtime/networkctl
-/bin/networkctl status ppp0 > /tmp/snapshot/runtime/networkctl-ppp0
-/usr/bin/systemd-cgtop --batch --iterations=1 --order=memory --cpu=time > /tmp/snapshot/runtime/systemd-cgtop-cpu-time
-/usr/bin/systemd-cgtop --batch --iterations=2 --order=cpu --cpu=percentage --delay=5s > /tmp/snapshot/runtime/systemd-cgtop-cpu-percentage
+/usr/bin/healthcheck  > "${SNAPSHOT_DIR}/runtime/healthcheck" 2>&1 || true
+/bin/networkctl > "${SNAPSHOT_DIR}/runtime/networkctl"
+/bin/networkctl status ppp0 > "${SNAPSHOT_DIR}/runtime/networkctl-ppp0"
+/usr/bin/systemd-cgtop --batch --iterations=1 --order=memory --cpu=time > "${SNAPSHOT_DIR}/runtime/systemd-cgtop-cpu-time"
+/usr/bin/systemd-cgtop --batch --iterations=2 --order=cpu --cpu=percentage --delay=5s > "${SNAPSHOT_DIR}/runtime/systemd-cgtop-cpu-percentage"
 
 ##################################
 # Systemd units                  #
@@ -76,25 +81,25 @@ for s in \
     swupdate-check.timer \
     swupdate-progress.service \
     sysupgrade.service \
-  ; do systemctl is-failed "${s}" >/dev/null && systemctl status "${s}" > "/tmp/snapshot/runtime/systemctl-status-${s}";
+  ; do systemctl is-failed "${s}" >/dev/null && systemctl status "${s}" > "${SNAPSHOT_DIR}/runtime/systemctl-status-${s}"
 done
 
 ##################################
 # User data                      #
 ##################################
-mkdir /tmp/snapshot/runtime/user/
-cp /etc/sysupgrade.conf /tmp/snapshot/runtime/user/
-/usr/bin/opkg list-installed > /tmp/snapshot/runtime/user/opkg-list-installed
+mkdir "${SNAPSHOT_DIR}/runtime/user/"
+cp /etc/sysupgrade.conf "${SNAPSHOT_DIR}/runtime/user/"
+/usr/bin/opkg list-installed > "${SNAPSHOT_DIR}/runtime/user/opkg-list-installed"
 
 ##################################
 # tcpdump files                  #
 ##################################
 if [ -d /tmp/tcpdump-sherlock ]; then
-    cp -r /tmp/tcpdump-sherlock /tmp/snapshot/runtime/
+    cp -r /tmp/tcpdump-sherlock "${SNAPSHOT_DIR}/runtime/"
 fi
 
 ##################################
 # Create tarball & delete files  #
 ##################################
-tar cfz "$(/sbin/fw_printenv -n gatewayid)_snapshot.tar.gz" ./snapshot
-rm -rf /tmp/snapshot
+tar cfz "${SNAPSHOT_TAR}" "./$(basename "${SNAPSHOT_DIR}")"
+rm -rf "${SNAPSHOT_DIR}"
--- recipes-support/snapshot/snapshot.bb
+++ recipes-support/snapshot/snapshot.bb
@@ -3,10 +3,13 @@ MAINTAINER = "Gardena GmbH"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
-RDEPENDS_${PN} += "healthcheck"
+RDEPENDS_${PN} += "\
+    busybox \
+    healthcheck \
+"
 
-PV = "1.8.0"
-PR = "r3"
+PV = "2.0.0"
+PR = "r0"
 
 SRC_URI = "\
     file://snapshot.sh \
