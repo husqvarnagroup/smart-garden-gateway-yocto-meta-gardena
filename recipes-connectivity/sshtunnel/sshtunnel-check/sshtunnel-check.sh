32064c21ee9a5abbba8a315c554c10dd9d87314f
Ezra Buehler <ezra.buehler@husqvarnagroup.com>
2024-04-09 21:12:57 +0200

    sshtunnel-check: Increase cURL timeout
    Requests may take longer than 5 seconds, e.g., due to slow DNS
    resolution. To be on the save side, drastically increase the timeout.
    
    Issue: SG-21269

--- recipes-connectivity/sshtunnel/sshtunnel-check/sshtunnel-check.sh
+++ recipes-connectivity/sshtunnel/sshtunnel-check/sshtunnel-check.sh
@@ -19,7 +19,7 @@ if ! gw_id_hash="$(fw_printenv -n gatewayid | tr -d '\n' | openssl sha1 | \
 fi
 
 return_code=0
-curl -sfI --max-time 5 "$ALLOWLIST/$gw_id_hash" >/dev/null || return_code=$?
+curl -sfI --max-time 30 "$ALLOWLIST/$gw_id_hash" >/dev/null || return_code=$?
 if [ "$return_code" -eq 22 ]; then  # >= 400
     exit 1
 elif [ "$return_code" -ne 0 ]; then
